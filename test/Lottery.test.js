91a558f24a12cdbd8832081b9208c0ace14b2a10
BFunk <bret.e.funk@gmail.com>
2018-02-06 17:38:04 -0500

    updated tests

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -3,14 +3,13 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const provider = ganache.provider();
 const web3 = new Web3(provider);
-const { interface, bytecode } = require('../compile.js');
+const { interface, bytecode } = require('../compile.js')
 
-let lottery;
 let accounts;
+let lottery;
 
 beforeEach(async () => {
   accounts = await web3.eth.getAccounts();
-
   lottery = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode })
     .send({ from: accounts[0], gas: 1000000 })
